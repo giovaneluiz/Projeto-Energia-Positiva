81799d06342e640e73a71e665a7c1956179e97fc
Luan <luanchristian.rochadocarmo@gmail.com>
2023-06-26 19:27:59 -0300

    refactor: mudança de titulo

--- projeto/src/components/session/Session.tsx
+++ projeto/src/components/session/Session.tsx
@@ -2,7 +2,7 @@ import './style.css'
 export const Session = () => {
   return (
     <section className="container-session" id='home'>
-      <h1>Projeto Energia Limpa</h1>
+      <h1>Projeto Energia Positiva</h1>
       <p>Energia sustentável para todos</p>
     </section>
   )
