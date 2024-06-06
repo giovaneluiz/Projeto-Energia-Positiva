2b70ea394de1cfd4b3461ae3c6f05173530c9b86
giovaneluiz <giovaneluiz19@yahoo.com.br>
2024-06-06 00:53:12 -0300

    adição de assinatura de newsletter

--- projeto/src/components/footer/Footer.tsx
+++ projeto/src/components/footer/Footer.tsx
@@ -2,7 +2,18 @@ import "./style.css"
 export const Footer = () => {
   return (
     <footer>
-      <p>
+      <div className="newsletter">
+        <h1>Assine nossa Newsletter</h1>
+        <p>Descubra como estamos transformando o mundo com energia limpa e sustentável para todos.</p>
+        <input className="email" type="email" placeholder="Insira seu e-mail" />
+        <div className="group-police">
+          <input type="checkbox" name="police" id="police" />
+          <label htmlFor="police">Declaro que li e aceito a <a href="/politica-de-privacidade">Politica de Privacidade.</a></label>
+        </div>
+        <button className="btn-newsletter">Assinar</button>
+      </div>
+      <hr />
+      <p className="copy">
         &copy; 2024 Todos os direitos reservados | Desenvolvido por alunos do grupo 4 de UPX
       </p>
     </footer>
