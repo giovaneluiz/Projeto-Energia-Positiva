b3196a7e9c8ae0ebf4463ff977a1e61dbfdca8fc
giovaneluiz <giovaneluiz19@yahoo.com.br>
2024-06-06 00:22:57 -0300

    inclusão de campo para telefone e script para rodar na rede para acesso de outro dispositivo em rede

--- projeto/src/components/session-third/SessionThird.tsx
+++ projeto/src/components/session-third/SessionThird.tsx
@@ -3,11 +3,12 @@ import { toast } from "react-toastify";
 import "./style.css"
 export const SessionThird = () => {
   const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
   const [email, setEmail] = useState('')
   const [desc, setDesc] = useState('')
 
   const handleInvite = () => {
-    if (name !== '' && email !== '' && desc !== '') {
+    if (name !== '' && phone !== '' && email !== '' && desc !== '' ) {
       toast.success("Solicitação enviada com sucesso, aguarde o contato de um representante!", {
         position: "top-right",
         autoClose: 5000,
@@ -21,6 +22,7 @@ export const SessionThird = () => {
       setDesc('')
       setEmail('')
       setName('')
+      setPhone('')
     } else {
       toast.error("Ops! Algo deu errado, confira se todos os campos estão preenchidos.", {
         position: "top-right",
@@ -43,6 +45,10 @@ export const SessionThird = () => {
           <div className="input-group">
             <label> Nome: </label>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+          </div>
+          <div className="input-group">
+            <label> Telefone/WhatsApp: </label>
+            <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} required />
           </div>
           <div className="input-group">
             <label> Email: </label>
