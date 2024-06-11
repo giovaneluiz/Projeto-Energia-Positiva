0bcb76243fedc4892e2fb77ba34d65d9b1fb19d0
giovaneluiz <giovaneluiz19@yahoo.com.br>
2024-06-11 12:52:51 -0300

    inclusao da opção "blog"

--- projeto/src/components/navbar/NavBar.tsx
+++ projeto/src/components/navbar/NavBar.tsx
@@ -5,9 +5,10 @@ export const NavBar = () => {
   return (
     <div className="container-navbar">
       <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">Sobre</a></li>
-        <li><a href="#contact">Contato</a></li>
+        <li><a href="/#home">Home</a></li>
+        <li><a href="/#about">Sobre</a></li>
+        <li><a href="/#contact">Contato</a></li>
+        <li><a href="/blog">Blog</a></li>
       </ul>
     </div>
   )
