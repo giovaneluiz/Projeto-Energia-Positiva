2c2ee93526fc3c74448de6dfb33dceff867d9c15
giovaneluiz <giovaneluiz19@yahoo.com.br>
2024-06-12 21:33:02 -0300

    alteração da rota de <a> para <Link>

--- projeto/src/components/navbar/NavBar.tsx
+++ projeto/src/components/navbar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import "./style.css"
 
 
@@ -8,7 +9,7 @@ export const NavBar = () => {
         <li><a href="/#home">Home</a></li>
         <li><a href="/#about">Sobre</a></li>
         <li><a href="/#contact">Contato</a></li>
-        <li><a href="/blog">Blog</a></li>
+        <li><Link to="/blog">Blog</Link></li>
       </ul>
     </div>
   )
